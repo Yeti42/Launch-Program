c2e155f0787aabb14751f1411524034ad0848664
Electricjacob11 <electricjacob11@gmail.com>
2024-12-09 09:36:58 -0800

    Some card word

--- Games/LiveGames/Scripts/WarScript.js
+++ Games/LiveGames/Scripts/WarScript.js
@@ -1,8 +1,11 @@
 const baseDeck = [];
-let p1Deck = [];
-let p2Deck = [];
+let p1Deck = [1, 2, 3, 4]; // TODO: set this correctly
+let p2Deck = [4, 3, 2, 1];
 let card1 = 0;
 let card2 = 0;
+let war1 = [];
+let war2 = [];
+let wasEqual = false;
 
 /**
  * Draw a card for the player's deck
@@ -19,10 +22,59 @@ function draw() {
     text2.textContent = card2;
 
     if (card1 > card2) {
-        headText.textContent = "Player has the higher card!";
+        headText.textContent = "You have the higher card!";
+        processCards(1);
     } else if (card1 < card2) {
         headText.textContent = "Computer has the higher card!";
+        processCards(2);
     } else if (card1 === card2) {
-        headText.textContent = "Cards are equal!";
+        headText.textContent = "War!";
+        processCards(3);
+    }
+}
+
+function processCards(cardCheck) {
+    setTimeout(() => {
+        switch (cardCheck) {
+            case 1:
+                p1Deck.push(card1);
+                p1Deck.push(card2);
+                break;
+
+            case 2:
+                p2Deck.push(card2);
+                p2Deck.push(card1);
+                break;
+
+            case 3:
+                wasEqual = true;
+                equalCards();
+                break;
+        }
+    }, 1000);
+}
+
+function equalCards() {
+    war1.unshift(p1Deck.shift());
+    war1.unshift(p1Deck.shift());
+    war2.unshift(p2Deck.shift());
+    war2.unshift(p2Deck.shift());
+
+    const text1 = document.getElementById("p1");
+    const text2 = document.getElementById("p2");
+    const headText = document.getElementById("head");
+
+    text1.textContent = war1.at(0);
+    text2.textContent = war2.at(0);
+
+    if (war1.at(0) > war2.at(0)) {
+        headText.textContent = "You have the higher card!";
+        processCards(1);
+    } else if (war1.at(0) < war2.at(0)) {
+        headText.textContent = "Computer has the higher card!";
+        processCards(2);
+    } else if (war1.at(0) === war2.at(0)) {
+        headText.textContent = "War, again!";
+        processCards(3);
     }
 }

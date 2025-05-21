3ddbc350f06d0216d09c660fbb47248abbf6d9ab
Electricjacob11 <electricjacob11@gmail.com>
2025-05-20 22:02:17 -0700

    More concentration

---
+++ Games/LiveGames/Scripts/Concentration.js
@@ -0,0 +1,5 @@
+const lowercase = [...Array(26)].map((_, i) => String.fromCharCode(97 + i));
+const uppercase = [...Array(26)].map((_, i) => String.fromCharCode(65 + i));
+
+console.log("Lowercase:", lowercase);
+console.log("Uppercase:", uppercase);
